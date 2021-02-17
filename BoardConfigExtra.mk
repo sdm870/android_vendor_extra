a87bda2fa12efd73db49f53bae6374f79274ac0f
Nolen Johnson <johnsonnolen@gmail.com>
2021-02-17 17:51:55 -0500

    extra: Initial commit

---
+++ BoardConfigExtra.mk
@@ -0,0 +1,9 @@
+# Product
+ifneq ($(filter %bonito %sargo %crosshatch %blueline %flame %coral,$(TARGET_PRODUCT)),)
+    TARGET_COPY_OUT_PRODUCT := product
+else
+    TARGET_COPY_OUT_PRODUCT := system/product
+endif
+
+## SELinux
+BOARD_SEPOLICY_DIRS += vendor/extra/sepolicy
