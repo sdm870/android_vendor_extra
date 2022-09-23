39ea4e237622446f1631c5c9492cde1f3636963e
Mohammed Althaf Thayyil <althafvly@gmail.com>
2022-09-23 17:37:48 +0530

    Fetch raw json for ota

--- product.mk
+++ product.mk
@@ -47,7 +47,7 @@ endif
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 
 ## Recovery
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += lineage.updater.uri="https://dl.althafvly.in/s/lineage_{device}"
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += lineage.updater.uri="https://dl.althafvly.in/s/lineage_{device}/download"
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.recovery_update=true
 
 ## Signing
